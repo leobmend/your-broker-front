14309319f5262d64cd63eca680dc745c58d8bbd0
leobmend <leo.bmendonca@gmail.com>
2022-07-24 15:42:21 -0300

    refactor: minor change on your broker API util

--- src/utils/your.broker.api.js
+++ src/utils/your.broker.api.js
@@ -12,13 +12,13 @@ const api = axios.create({
 
 const login = async ({ email, senha }) => (
   api.post('/credenciais/entrar', { email, senha })
-    .then(({ data }) => ({ data }))
+    .then(({ data }) => ({ token: data.token }))
     .catch(({ response }) => ({ error: response.data.mensagem }))
 );
 
 const signUp = async ({ nome, email, senha }) => (
   api.post('/credenciais/cadastrar', { nome, email, senha })
-    .then(({ data }) => ({ data }))
+    .then(({ data }) => ({ token: data.token }))
     .catch(({ response }) => ({ error: response.data.mensagem }))
 );
 
@@ -26,7 +26,7 @@ const cliente = async (token) => (
   api.get(
     `/clientes/${getCodClienteFromToken(token)}`,
     { headers: { Authorization: `bearer ${token}` } },
-  ).then(({ data }) => ({ data }))
+  ).then(({ data }) => ({ data: data.data }))
     .catch(({ response }) => ({ error: response.data.mensagem }))
 );
 
@@ -34,13 +34,37 @@ const investimentos = async (token) => (
   api.get(
     `/clientes/${getCodClienteFromToken(token)}/investimentos`,
     { headers: { Authorization: `bearer ${token}` } },
-  ).then(({ data }) => ({ data }))
+  ).then(({ data }) => ({ data: data.data }))
     .catch(({ response }) => ({ error: response.data.mensagem }))
 );
 
 const ativos = async ({ search = '', pag = 1 }) => (
   api.get(`/ativos?termo=${search}&pag=${pag}`)
-    .then(({ data }) => ({ data }))
+    .then(({ data }) => ({ data: data.data }))
+    .catch(({ response }) => ({ error: response.data.mensagem }))
+);
+
+const ativo = async (codAtivo) => (
+  api.get(`/ativos/${codAtivo}`)
+    .then(({ data }) => ({ data: data.data }))
+    .catch(({ response }) => ({ error: response.data.mensagem }))
+);
+
+const transacao = async (token, { valor, tipo }) => (
+  api.post(
+    `/clientes/${getCodClienteFromToken(token)}/transacoes`,
+    { valor, tipo },
+    { headers: { Authorization: `bearer ${token}` } },
+  ).then(({ data }) => ({ data: data.data }))
+    .catch(({ response }) => ({ error: response.data.mensagem }))
+);
+
+const operacao = async (token, { codAtivo, qtdeAtivo, tipo }) => (
+  api.post(
+    `/clientes/${getCodClienteFromToken(token)}/operacoes`,
+    { codAtivo, qtdeAtivo, tipo },
+    { headers: { Authorization: `bearer ${token}` } },
+  ).then(({ data }) => ({ data: data.data }))
     .catch(({ response }) => ({ error: response.data.mensagem }))
 );
 
@@ -50,6 +74,9 @@ const yourBrokerAPI = {
   cliente,
   investimentos,
   ativos,
+  ativo,
+  transacao,
+  operacao,
 };
 
 export default yourBrokerAPI;
